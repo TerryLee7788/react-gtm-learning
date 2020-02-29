8f63710159b72db1007637798414db18dd2aecf9
TerryLee <k9114658@gmail.com>
2020-02-29 14:54:10 +0800

    Add gtm learning code.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TagManager from 'react-gtm-module'
+
+Object.defineProperty(window, 'pageId', {
+  value: 33456,
+  writable: false
+})
 
 function App() {
+  const tagManagerArgs = {
+      gtmId: 'GTM-53G7WHF'
+  }
+
+  TagManager.initialize(tagManagerArgs)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="">
+      <ul>
+        <li data-id="111">list</li>
+        <li className="terry">page id</li>
+      </ul>
+    </header>
   );
 }
 
