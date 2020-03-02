db01f0bdb3e9931ec660835a70399ae60ee20bbe
TerryLee <Terry.lee@ehsn.com.tw>
2020-03-02 16:21:56 +0800

    Update code

--- src/App.js
+++ src/App.js
@@ -7,18 +7,56 @@ Object.defineProperty(window, 'pageId', {
   writable: false
 })
 
+const list = [
+  {
+    itemId: 9453,
+    name: 'terry1'
+  },
+  {
+    itemId: 9454,
+    name: 'terry2'
+  },
+  {
+    itemId: 9455,
+    name: 'terry3'
+  },
+]
+
 function App() {
   const tagManagerArgs = {
-      gtmId: 'GTM-53G7WHF'
+    gtmId: 'GTM-53G7WHF'
   }
 
   TagManager.initialize(tagManagerArgs)
+
+  // 直播間編號
+  window.dataLayer.push({
+    pageId: '直播間編號: 3345678'
+  })
+
   return (
     <header className="">
       <ul>
-        <li data-id="111">list</li>
-        <li className="terry">page id</li>
+        <li className="list" data-id="111">list</li>
+        {
+          list.map((obj) => (
+            <li className="terry"
+              onClick={() => {
+
+                window.dataLayer.push({
+                  pageId: obj.itemId,
+                  event: 'terryClicked'
+                })
+
+              }}
+            >{obj.name}
+            </li>
+          ))
+        }
       </ul>
+      <button
+        className="checkoutBtn"
+      >結帳</button>
     </header>
   );
 }
